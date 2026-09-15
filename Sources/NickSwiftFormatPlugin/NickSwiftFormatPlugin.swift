fa8422fdecf02cfb1a166b3dc6bc1bbfb2ded59b
palinasupranovich <palina.supranovich@mend.io>
2026-09-15 09:36:23 +0200

    Add Sources/NickSwiftFormatPlugin/NickSwiftFormatPlugin.swift (generated test project)

---
+++ Sources/NickSwiftFormatPlugin/NickSwiftFormatPlugin.swift
@@ -0,0 +1,10 @@
+// Minimal command plugin stub.
+import PackagePlugin
+
+@main
+struct NickSwiftFormatPlugin: CommandPlugin {
+    func performCommand(
+        context: PluginContext,
+        arguments: [String]
+    ) async throws {}
+}
